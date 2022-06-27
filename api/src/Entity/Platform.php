ce58c71cc645baf2b7fab8b95a2adc00896d8c67
eyramds <eyram.souza@gmail.com>
2022-06-27 11:27:36 +0200

    custom search filter

--- api/src/Entity/Platform.php
+++ api/src/Entity/Platform.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use App\Filter\CustomSearchFilter;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
 use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
@@ -27,6 +28,7 @@ use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
     ]
 )]
 #[ApiFilter(PropertyFilter::class)]
+#[ApiFilter(CustomSearchFilter::class)]
 class Platform
 {
     #[ORM\Id]
