212d417ce7abca326125755839f24c2915be2b58
nassim95 <nasfahdine@gmail.com>
2022-06-30 22:05:45 +0200

    allow cors

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 
 use App\Kernel;
 
+if((!empty( $_SERVER['HTTP_X_FORWARDED_HOST'])) || (!empty( $_SERVER['HTTP_X_FORWARDED_FOR'])) ) {
+    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    $_SERVER['HTTPS'] = 'on';
+}
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
