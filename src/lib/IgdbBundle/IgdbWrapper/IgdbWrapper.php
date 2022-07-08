c514e46d76a49e3e471265dcda7fa0a83e440bbb
nassim95 <nasfahdine@gmail.com>
2022-07-08 10:21:06 +0200

    remove company igdb

--- src/lib/IgdbBundle/IgdbWrapper/IgdbWrapper.php
+++ src/lib/IgdbBundle/IgdbWrapper/IgdbWrapper.php
@@ -61,12 +61,12 @@ class IgdbWrapper
         $response = $this->getPlatforms();
         $this->serializeDatas($response,PlatformMapper::class);
 
-        $companiesCount = $this->countCompanies()/500;
-        for ($i=0; $i < $companiesCount; $i++) {  
-            $offset = ($i == 0)? 0 : $i*500;
-            $response = $this->getCompanies($offset);
-            $this->serializeDatas($response, CompanyMapper::class);
-        }
+        // $companiesCount = $this->countCompanies()/500;
+        // for ($i=0; $i < $companiesCount; $i++) {  
+        //     $offset = ($i == 0)? 0 : $i*500;
+        //     $response = $this->getCompanies($offset);
+        //     $this->serializeDatas($response, CompanyMapper::class);
+        // }
 
         $gameCount = $this->countGames()/500;
         for ($i=0; $i < $gameCount; $i++) {  
